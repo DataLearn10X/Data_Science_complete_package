5c574d9a613527b8cffff6b866188eba8839cd86
DataLearn10X <tushardeshmukh142@gmail.com>
2026-03-11 19:40:39 +0530

    Include all available ad folder images in slider config

--- config.js
+++ config.js
@@ -2,10 +2,16 @@ window.DL10X_CONFIG = {
   SHEET_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzOObmcsyQcTFxzXhYMegyAXRWyFX2cmjhdCaToNZr1iDcLW2Rywx29qVoViNy_Gu_r/exec",
   WHATSAPP_NUMBER: "919403030512",
   WHATSAPP_TEXT: "Hi DataLearn10X, I want details about courses",
-  AD_IMAGES_BASE_URL: "",
+  AD_IMAGES_BASE_URL: "ads",
   AD_IMAGES: [
-    "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?auto=format&fit=crop&w=1400&q=80",
-    "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&w=1400&q=80",
-    "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&w=1400&q=80"
+    "ad1.jpg",
+    "ad2.jpg",
+    "1 (1).png",
+    "ChatGPT Image Mar 9, 2026, 06_39_03 PM.png",
+    "ChatGPT Image Mar 9, 2026, 06_39_14 PM.png",
+    "ChatGPT Image Mar 9, 2026, 06_53_34 PM.png",
+    "ChatGPT Image Mar 9, 2026, 07_00_53 PM.png",
+    "AD_IMAGES/ad1.jpg",
+    "AD_IMAGES/ad2.jpg"
   ]
 };
